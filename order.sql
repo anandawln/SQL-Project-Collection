62dc497827eb0d6695b82a24808bbd65c523c874
Wulan <anandawulan127@gmail.com>
2025-09-01 20:41:19 +0700

    postgresql

---
+++ order.sql
@@ -0,0 +1,12 @@
+--select * from product_category pc
+
+--select MAX(pc."Quantity") from product_category pc
+
+--select MIN(pc."Quantity") from product_category pc 
+
+--select count(distinct pc."ProdNumber") from product_category pc 
+--where pc."Quantity" = 6 
+
+--select pc."ProdNumber" from product_category pc
+--where pc."Quantity" = 6
+
